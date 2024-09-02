2f7a5abea27b758dac0ee352ceaf0c74257168f9
VinceBmmrt <vincebommert75@gmail.com>
2024-09-02 23:00:36 +0200

    test

--- nasa_info/urls.py
+++ nasa_info/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('api/apod/', views.apod_view, name='apod_json'),
     path('', views.index_view, name='index'),
     path('planets/', views.planets_view, name='planets'),
-    path('api/planet-info/<str:planet_name>/', views.planet_info_view, name='planet_info'),
+    # path('api/planet-info/<str:planet_name>/', views.planet_info_view, name='planet_info'),
 ]
