addcc9bfab669f3f8b54a2b58b1ac6fd0917556c
VinceBmmrt <vincebommert75@gmail.com>
2024-09-02 23:29:12 +0200

    refacto

--- nasa_project/settings.py
+++ nasa_project/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Utilisation de la clé API dans les paramètres
 NASA_API_KEY = config('NASA_API_KEY', default='default_value_if_missing')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hxh5onuu835afp=l-_utjvrd+qt03%8p^=kte#n3_@v7ge*zvh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
